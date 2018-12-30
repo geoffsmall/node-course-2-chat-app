58a6d0ddc9a5772631080034703294465a7658c9
Geoff Small <gsmall@superheatfgh.com>
2018-12-30 07:23:14 -0500

    Add form for message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -3,24 +3,32 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Connected to server');
 
-    socket.emit('createMessage',{
-        from: 'geoff@example.com',
-        text: 'Creating a message'
-    })
+    // socket.emit('createMessage',{
+    //     from: 'geoff@example.com',
+    //     text: 'Creating a message'
+    // })
 });
 
 socket.on('disconnect', function(){
     console.log('Disconnected to server');
 });
 
-/*
-socket.on('newEmail', function(email){
-    console.log('New email', email);
+socket.on('newMessage', function(message){
+    console.log('newMessage',message);
+
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
-*/
 
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
 
+    socket.emit('createMessage', {
+        from:'User',
+        text: jQuery('[name=message]').val()
+    }, function(){
 
-socket.on('newMessage', function(newMessage){
-    console.log('newMessage',newMessage);
-})
+    })
+});
