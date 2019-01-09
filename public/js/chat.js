81b7bb149edfd4a04f92de244aff748e382657a9
Geoff Small <gsmall@superheatfgh.com>
2019-01-09 05:47:36 -0500

    Send message to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,17 +19,32 @@ function scrollToBottom () {
   }
 
 socket.on('connect', function(){
-    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
 
-    // socket.emit('createMessage',{
-    //     from: 'geoff@example.com',
-    //     text: 'Creating a message'
-    // })
+    socket.emit('join', params, function(err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('No error');
+        }
+    });
 });
 
 socket.on('disconnect', function(){
     console.log('Disconnected to server');
 });
+
+socket.on('updateUserList', function(users){
+    console.log('Users List', users);
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function(user){
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+})
 
 socket.on('newMessage', function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -63,7 +78,6 @@ jQuery('#message-form').on('submit', function(e){
     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from:'User',
         text: jQuery('[name=message]').val()
     }, function(){
         messageTextbox.val('');
